038249229d087957f8b799b8a95cdda463851a0e
alekamaleka <taalaibekovaaielita@gmail.com>
2025-09-29 18:34:11 +0600

    contact us 80%

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import ContactPage from './pages/ContactPage/ContactPage'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+        <ContactPage />
+    </div>
   )
 }
 
