c0c9287d4f61e5904966b95a8a0cd0a763ef852a
Akjol <akjolabdilazisov@gmail.com>
2025-09-30 19:16:56 +0600

    akjol 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-=======
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -21,5 +11,4 @@ createRoot(document.getElementById("root")).render(
       <App />
     </BrowserRouter>
   </StrictMode>
->>>>>>> b2cff71af1931c246759c78fe699d1493e2374a8
 );
